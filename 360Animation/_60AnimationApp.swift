f25a4a72daae0d6e3770ba9e4c2c0174a3cbb7c0
STRIKE <hmk1137@naver.com>
2022-09-29 18:49:32 +0900

    Initial Commit

---
+++ 360Animation/_60AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _60AnimationApp.swift
+//  360Animation
+//
+//  Created by 하명관 on 2022/09/29.
+//
+
+import SwiftUI
+
+@main
+struct _60AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
